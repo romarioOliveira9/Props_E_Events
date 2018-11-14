a24dcd195ca1d9ff60acbead03e2f5acfb214f5e
Plínio Naves <pliniopjn@gmail.com>
2018-11-14 13:38:50 -0200

    Removendo código desnecessario

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import FilmesListaIten from './components/FilmesListaIten.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  components: {
-    FilmesListaIten
-  }
+  render: h => h(App)
 }).$mount('#app')
