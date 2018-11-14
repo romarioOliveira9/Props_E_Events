ca517de65905e4b67222f1240c0c71165b7b9b17
Plínio Naves <pliniopjn@gmail.com>
2018-11-14 14:35:50 -0200

    Tipagem em props

--- src/components/FilmesListaIten.vue
+++ src/components/FilmesListaIten.vue
@@ -7,11 +7,16 @@
 
 <script>
 export default {
-    props: ['filmeTitulo'],
+    props: {
+        filmeTitulo: String
+    },
     computed: {
         filmeTituloConcatenado() {
             return `Título: ${this.filmeTitulo}`
         }
+    },
+    created() {
+        console.log(typeof this.filmeTitulo)
     }
 }
 </script>
