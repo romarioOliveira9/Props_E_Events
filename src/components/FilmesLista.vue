fc97cda9703b3367a833a7d5e0fb97fb0d6e9292
Plínio Naves <pliniopjn@gmail.com>
2018-11-14 11:17:54 -0200

    Passando dados para props dinamicamente

--- src/components/FilmesLista.vue
+++ src/components/FilmesLista.vue
@@ -8,9 +8,10 @@
 
       <div class="list-group list-group-flush">
 
-        <FilmesListaIten filmeTitulo="Vingadores: Guerra Infinita" />
-        <FilmesListaIten filmeTitulo="Homem Formiga e a Vespa" />
-        <FilmesListaIten filmeTitulo="Pantera Negra" />
+        <FilmesListaIten
+          v-for="(filme, index) in filmes"
+          :key="index"
+          :filmeTitulo="filme" />
 
       </div>
     </div>
@@ -34,6 +35,16 @@ export default {
   components: {
     FilmesListaIten,
     FilmesListaItenInfo
+  },
+  data() {
+    return {
+      filmes: [
+        'Vingadores: Guerra Infinita',
+        'Homem Formiga e a Vespa',
+        'Pantera Negra',
+        'Deadpool 2'
+      ]
+    }
   }
 }
 </script>
